b4f57d62db7044ee6febc4b7738574599b9d7ebf
Calvindd2f <74103103+Calvindd2f@users.noreply.github.com>
2024-05-27 03:43:18 +0100

    improve

--- build.cmd
+++ build.cmd
@@ -1,32 +1,64 @@
-:: Download and build TA-Lib source code
+# Download and build TA-Lib source code
 @echo on
 
-:: set TALIB_C_VER=0.4.0
-:: set TALIB_PY_VER=0.4.29
-:: set VS_PLATFORM=x64
+# Set the version of TA-Lib and Visual Studio platform
+set TALIB_C_VER=0.4.0
+set TALIB_PY_VER=0.4.29
+set VS_PLATFORM=x64
 
-curl -L -o talib.zip https://sourceforge.net/projects/ta-lib/files/ta-lib/%TALIB_C_VER%/ta-lib-%TALIB_C_VER%-msvc.zip
-if errorlevel 1 exit /B 1
+# Download TA-Lib source code
+curl -L -o talib.zip "https://sourceforge.net/projects/ta-lib/files/ta-lib/%TALIB_C_VER%/ta-lib-%TALIB_C_VER%-msvc.zip"
+if errorlevel 1 (
+  echo Error downloading TA-Lib source code
+  exit /B 1
+)
 
-tar -xf talib.zip
-if errorlevel 1 exit /B 1
+7z x talib.zip -y
+if errorlevel 1 (
+  echo Error extracting TA-Lib source code
+  exit /B 1
+)
 
-del talib\_ta_lib.c
+del talib\ta_lib.c
 
-curl -L -o talib-python.zip https://github.com/TA-Lib/ta-lib-python/archive/refs/tags/TA_Lib-%TALIB_PY_VER%.zip
-if errorlevel 1 exit /B 1
+# Download TA-Lib-Python source code
+curl -L -o talib-python.zip "https://github.com/TA-Lib/ta-lib-python/archive/refs/tags/TA_Lib-%TALIB_PY_VER%.zip"
+if errorlevel 1 (
+  echo Error downloading TA-Lib-Python source code
+  exit /B 1
+)
 
-tar -xf talib-python.zip --strip-components=1
-if errorlevel 1 exit /B 1
+7z x talib-python.zip -y --strip-components=1
+if errorlevel 1 (
+  echo Error extracting TA-Lib-Python source code
+  exit /B 1
+)
 
-git apply --verbose --binary talib.diff
-if errorlevel 1 exit /B 1
+# Apply patch
+git --version >nul 2>&1 || (
+  echo Git is not installed. Skipping patch application.
+)
 
-:: set MSBUILDTREATHIGHERTOOLSVERSIONASCURRENT
+if exist talib-TA_Lib-%TALIB_PY_VER%\ta_lib\c\mfiles\ta_func.h (
+  git -C talib-TA_Lib-%TALIB_PY_VER% apply --verbose --binary talib.diff
+  if errorlevel 1 (
+    echo Error applying patch
+    exit /B 1
+  )
+)
 
-msbuild ta-lib\c\ide\vs2022\lib_proj\ta_lib.sln /m /t:Clean;Rebuild /p:Configuration=cdr /p:Platform=%VS_PLATFORM%
-if errorlevel 1 exit /B 1
+# Build TA-Lib
+msbuild talib-TA_Lib-%TALIB_PY_VER%\ta-lib\c\ide\vs2022\lib_proj\ta_lib.sln /m /t:Clean;Rebuild /p:Configuration=cdr /p:Platform=%VS_PLATFORM%
+if errorlevel 1 (
+  echo Error building TA-Lib
+  exit /B 1
+)
 
-copy /Y ta-lib\c\include\*.h ta-lib\
+copy /Y talib-TA_Lib-%TALIB_PY_VER%\ta-lib\c\include\*.h talib-TA_Lib-%TALIB_PY_VER%\
 
-:: del pyproject.toml
+# Clean up
+del talib.zip
+del talib-python.zip
+del talib-TA_Lib-%TALIB_PY_VER%\ta-lib\c\mfiles\*.obj
+del talib-TA_Lib-%TALIB_PY_VER%\ta-lib\c\mfiles\*.exp
+del talib-TA_Lib-%TALIB_PY_VER%\ta-lib\c\mfiles\*.lib
